b1d521d7bf5c64e25d43e061e58108fe20121780
Mohammad Azam <azamsharp@gmail.com>
2021-05-13 09:34:18 -0500

    Initial Commit

---
+++ CountriesApp/CountriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesAppApp.swift
+//  CountriesApp
+//
+//  Created by Mohammad Azam on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct CountriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
